1ef3d3effb3c72b24da25e681f202c305f37ff8e
raseshbiyani-5501P <72201706+raseshbiyani-5501P@users.noreply.github.com>
2022-08-23 17:48:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,35 @@
 function preload(){}
 
+
 function setup(){
 
 canvas = createCanvas(500, 450);
 canvas.center();
+video = createCapture(VIDEO);
+video.position(20 ,150);
+video.size(250 ,300);
+poseNet = ml5.poseNet(video, modelLoaded);
+poseNet.on("pose",gotPoses);
 
 }
 
-function draw(){}
+function draw(){
+
+background("sea-green")
+
+}
+
+function modelLoaded(){
+
+console.log('Model has been loaded');
+
+}
+function gotPoses(results){
+
+if(results.length > 0){
+
+    console.log(results);
+
+}
+
+}
